294f88de1c4027a88d7a21bb9971e544eac3ca34
samuel santos <samaraujo28@gmail.com>
2022-10-27 23:48:10 -0300

    feat(backend): implementado testes da camada de servicos do usuario

---
+++ back-end/src/tests/mocks/userMocks.js
@@ -0,0 +1,11 @@
+const userMock = {
+  id: 1,
+  email: 'ze_delivery@email.com',
+  name: 'Ze Delivery',
+  password: '71e227587b8a3ff3da9eb524e18185af', // password: deliveryPassword
+  role: 'customer'
+};
+
+module.exports = {
+  userMock,
+};
