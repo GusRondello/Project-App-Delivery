12088e2a3f24bf74906145cb272ea8b2705884bb
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-10-30 16:28:54 -0300

    improvement(Products Card): Implementa a lógica de alterar manualmente a quantidade de itens do produto

--- front-end/src/components/Products.js
+++ front-end/src/components/Products.js
@@ -1,12 +1,13 @@
 // Cria um componente que recebe os produtos do banco de dados e os renderiza na tela
 import React, { /* useEffect, useState,  */useContext } from 'react';
 import CustomerContext from '../context/CustomerContext';
+import ProductsCard from './ProductsCard';
 // import saveCartItem from '../helpers/saveCartItem';
 // import { getProducts } from '../services/apiAppDelivery';
 
 function Products() {
-  const { productsArray, setProductsArray,
-    setIsCartUpdated } = useContext(CustomerContext);
+  const { productsArray, /* setProductsArray,
+    setIsCartUpdated */ } = useContext(CustomerContext);
 
   // const handleCartOnLocalStorage = (newProductsQtd) => {
   //   const cart = newProductsQtd.map((product) => {
@@ -31,57 +32,29 @@ function Products() {
   // };
 
   // função que recebe o produto e adicionar ou subtratir e, modifica no estado a nova quantidade
-  const handleQuantity = (product, operation) => {
-    const newProductsQtd = productsArray.map((item) => {
-      if (item.id === product.id) {
-        const newQuantity = operation === 'add' ? item.quantity + 1 : item.quantity - 1;
-        if (newQuantity < 0) return { ...item, quantity: 0 };
-        return { ...item, quantity: newQuantity };
-      }
-      return item;
-    });
-    // handleCartOnLocalStorage(newProductsQtd);
-    setProductsArray(newProductsQtd);
-    setIsCartUpdated(true);
-  };
+  // const handleQuantity = (product, operation) => {
+  //   const newProductsQtd = productsArray.map((item) => {
+  //     if (item.id === product.id) {
+  //       const newQuantity = operation === 'add' ? item.quantity + 1 : item.quantity - 1;
+  //       if (newQuantity < 0) return { ...item, quantity: 0 };
+  //       return { ...item, quantity: newQuantity };
+  //     }
+  //     return item;
+  //   });
+  //   // handleCartOnLocalStorage(newProductsQtd);
+  //   setProductsArray(newProductsQtd);
+  //   setIsCartUpdated(true);
+  // };
 
   return (
     <div>
-      {productsArray?.map((product) => (
-        <div key={ product.id }>
-          <p data-testid={ `customer_products__element-card-title-<${product.id}>` }>
-            {product.name}
-          </p>
-          <p data-testid={ `customer_products__element-card-price-<${product.id}>` }>
-            {product.price}
-          </p>
-          <p data-testid={ `customer_products__img-card-bg-image-<${product.id}>` }>
-            <img src={ product.url_image } alt={ product.name } />
-          </p>
-          <div>
-            {/* Botão para diminuir quantidade de itens; */}
-            <button
-              type="button"
-              data-testid={ `customer_products__button-card-rm-item-<${product.id}>` }
-              onClick={ () => handleQuantity(product, 'remove') }
-            >
-              -
-            </button>
-            {/* Elemento que exibe a quantidade de itens atual do produto */}
-            <p data-testid={ `customer_products__input-card-quantity-<${product.id}>` }>
-              {product.quantity}
-            </p>
-            {/* Botão para adicionar quantidade de itens; */}
-            <button
-              type="button"
-              data-testid={ `customer_products__button-card-add-item-<${product.id}>` }
-              onClick={ () => handleQuantity(product, 'add') }
-            >
-              +
-            </button>
+      <div>
+        {productsArray?.map((product) => (
+          <div key={ product.id }>
+            <ProductsCard product={ product } />
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
---
+++ front-end/src/components/ProductsCard.js
@@ -0,0 +1,84 @@
+import React, { useContext } from 'react';
+import propTypes from 'prop-types';
+import CustomerContext from '../context/CustomerContext';
+
+function ProductsCard({ product }) {
+  const { productsArray, setProductsArray,
+    setIsCartUpdated } = useContext(CustomerContext);
+
+  const handleQuantity = (operation) => {
+    const newProductsQtd = productsArray.map((item) => {
+      if (item.id === product.id) {
+        const newQuantity = operation === 'add' ? item.quantity + 1 : item.quantity - 1;
+        if (newQuantity < 0) return { ...item, quantity: 0 };
+        return { ...item, quantity: newQuantity };
+      }
+      return item;
+    });
+    setProductsArray(newProductsQtd);
+    setIsCartUpdated(true);
+  };
+
+  return (
+    <div>
+      <p data-testid={ `customer_products__element-card-title-<${product.id}>` }>
+        {product.name}
+      </p>
+      <p data-testid={ `customer_products__element-card-price-<${product.id}>` }>
+        {product.price}
+      </p>
+      <p data-testid={ `customer_products__img-card-bg-image-<${product.id}>` }>
+        <img src={ product.url_image } alt={ product.name } />
+      </p>
+      <div>
+        {/* Botão para diminuir quantidade de itens; */}
+        <button
+          type="button"
+          data-testid={ `customer_products__button-card-rm-item-<${product.id}>` }
+          onClick={ () => handleQuantity('remove') }
+        >
+          -
+        </button>
+        {/* Elemento que exibe a quantidade de itens e permite também definir outro valor */}
+        <input
+          type="number"
+          data-testid={ `customer_products__input-card-quantity-<${product.id}>` }
+          value={ product.quantity }
+          onChange={ (e) => {
+            const newProductsQtd = productsArray.map((item) => {
+              if (item.id === product.id) {
+                return { ...item, quantity: Number(e.target.value) };
+              }
+              return item;
+            });
+            setProductsArray(newProductsQtd);
+            setIsCartUpdated(true);
+          } }
+        />
+        {/* <p data-testid={ `customer_products__input-card-quantity-<${product.id}>` }>
+          {product.quantity}
+        </p> */}
+        {/* Botão para adicionar quantidade de itens; */}
+        <button
+          type="button"
+          data-testid={ `customer_products__button-card-add-item-<${product.id}>` }
+          onClick={ () => handleQuantity('add') }
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+ProductsCard.propTypes = {
+  product: propTypes.shape({
+    id: propTypes.number.isRequired,
+    name: propTypes.string.isRequired,
+    price: propTypes.number.isRequired,
+    url_image: propTypes.string.isRequired,
+    quantity: propTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default ProductsCard;
