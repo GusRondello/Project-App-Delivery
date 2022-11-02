2bf74de0072448af92afbbd4987e0ac56eb05a61
samuel santos <samaraujo28@gmail.com>
2022-11-02 20:40:29 -0300

    refact(backend): retirado password do retorno dos vendedores

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -42,6 +42,7 @@ const create = async (userData) => {
 const getSellers = async () => {
   const sellers = await User.findAll({
     where: { role: 'seller' },
+    attributes: { exclude: ['password'] },
   });
 
   return sellers;
