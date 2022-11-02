7e1e7847f3272de45f095a6bb0181d3d8cf8f706
Lais Simenikim <l.simenikim@gmail.com>
2022-11-02 18:39:06 -0300

    feat(frontend): implementando validação no form de novo usuário

--- front-end/src/pages/AdminManage.js
+++ front-end/src/pages/AdminManage.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import SelectField from '../components/SelectField';
 import TextField from '../components/TextField';
+import { validateEmail, validateMinLength } from '../helpers/validators';
+
+const MIN_NAME_LENGTH = 12;
+const MIN_PASSWORD_LENGTH = 6;
 
 function AdminManage() {
+  const [form, setForm] = useState({
+    valid: false,
+    values: {
+      name: '',
+      email: '',
+      password: '',
+      type: 'customer',
+    },
+    errors: {
+      name: true,
+      email: true,
+      password: true,
+      type: false,
+    },
+  });
+
+  function handleFormChange(name, validator) {
+    return (e) => {
+      const { value } = e.target;
+
+      setForm((state) => {
+        const error = validator ? !validator(value) : false;
+        const errors = { ...state.errors, [name]: error };
+        const valid = Object.values(errors).every((it) => it === false);
+
+        return {
+          valid,
+          errors,
+          values: { ...state.values, [name]: value },
+        };
+      });
+    };
+  }
+
   return (
     <div>
       <Header />
@@ -14,23 +52,29 @@ function AdminManage() {
             name="name"
             label="Nome"
             testId="admin_manage__input-name"
-            value=""
-            onChange={ () => {} }
+            value={ form.values.name }
+            onChange={ handleFormChange(
+              'name',
+              validateMinLength(MIN_NAME_LENGTH),
+            ) }
           />
           <TextField
             name="email"
             label="Email"
             testId="admin_manage__input-email"
-            value=""
-            onChange={ () => {} }
+            value={ form.values.email }
+            onChange={ handleFormChange('email', validateEmail) }
           />
           <TextField
             type="password"
             name="password"
             label="Senha"
             testId="admin_manage__input-password"
-            value=""
-            onChange={ () => {} }
+            value={ form.values.password }
+            onChange={ handleFormChange(
+              'password',
+              validateMinLength(MIN_PASSWORD_LENGTH),
+            ) }
           />
           <SelectField
             name="type"
@@ -41,10 +85,14 @@ function AdminManage() {
               { value: 'seller', label: 'Vendedor' },
               { value: 'customer', label: 'Cliente' },
             ] }
-            value=""
-            onChange={ () => {} }
+            value={ form.values.type }
+            onChange={ handleFormChange('type') }
           />
-          <button type="submit" data-testid="admin_manage__button-register">
+          <button
+            disabled={ !form.valid }
+            type="submit"
+            data-testid="admin_manage__button-register"
+          >
             Cadastrar
           </button>
         </fieldset>
